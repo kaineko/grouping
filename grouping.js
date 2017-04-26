adbf73687d01d0624600a0333a244f192488ce7e
kaineko <chanodejs@yahoo.co.jp>
2017-04-27 04:19:39 +0900

    Create grouping.js

---
+++ grouping.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$('.tasks').sortable({
+		connectWith:'.tasks',
+		placeholder:'on_tasks'
+	});
+	
+		$('body').mouseup(function(){
+			setTimeout(check,100);
+			function check(){
+				if(($('#fungus').find('.fungus').length) === 5){
+					if(($('#spirochaete').find('.spirochaete').length) === 3){
+						if(($('#protozoan').find('.protozoan').length) === 5){
+							alert('clear');
+						}
+					}		
+				}
+			}
+		})
+})
