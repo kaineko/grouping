794e33d44cdb7eb7aa53d4e3f9c43a1f55f2ffd5
kaineko <chanodejs@yahoo.co.jp>
2017-05-01 21:54:57 +0900

    Update grouping.js

--- grouping.js
+++ grouping.js
@@ -1,19 +1,24 @@
 $(document).ready(function(){
-	$('.tasks').sortable({
-		connectWith:'.tasks',
-		placeholder:'on_tasks'
-	});
-	
-		$('body').mouseup(function(){
-			setTimeout(check,100);
-			function check(){
-				if(($('#fungus').find('.fungus').length) === 5){
-					if(($('#spirochaete').find('.spirochaete').length) === 3){
-						if(($('#protozoan').find('.protozoan').length) === 5){
-							alert('正解です！お疲れさまでした！');
-						}
-					}		
-				}
-			}
-		})
+    var w = $(window).width();
+    var h = $(window).height();
+    
+    $('#left').width(w*20/100);
+    $('#left').height(h);
+    
+    $('#menu').click(function(){
+        if(left.style.display === 'block') {
+        	left.style.display = 'none';
+  		} else {
+    		left.style.display = 'block';
+        }
+    });
+    $('#homepage').click(function(){
+        window.location.href = 'index.html';
+    })
+    $('#bacteriumpage').click(function(){
+        window.location.href = 'Bacterium.html';
+    })
+    $('#otherspage').click(function(){
+        window.location.href = 'others.html';
+    })
 })
