9fd50e80c1bc982927cee3f897ed62549e9b34c4
kaineko <chanodejs@yahoo.co.jp>
2017-05-02 04:25:18 +0900

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ http.createServer(function(request,response){
 	var fullpath = path.resolve(__dirname,'.'+x.pathname);
 	if (fs.existsSync(fullpath)){
 		var ext = path.extname(fullpath).toLowerCase();
-		if(ext.match(/\.(png|jpg|jpeg|gif|html|css|js|ico)$/)){
+		if(ext.match(/\.(png|jpg|jpeg|gif|html|css|js)$/)){
 		   response.writeHead(200,{'Content-type':'text/html'});
 		   var strm = fs.createReadStream(fullpath);
 			strm.pipe(response);
